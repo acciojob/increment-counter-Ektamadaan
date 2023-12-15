04bbeca944cec02098138974e920c3c50bb3b0af
Ektamadaan <ektamadaan45@gmail.com>
2023-12-15 21:05:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const counterElement = document.getElementById('counter');
+
+const incrementButton = document.getElementById('incrementBtn');
+let counter = 0 ;
+function updateCounter() {
+	counter++ ;
+	counterElement.textContent = counter;
+}
+incrementButton.addEventListener('click' , updateCounter);
